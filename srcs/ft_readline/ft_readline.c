878d1a802dc81d04b291776654d43af1f505b39e
IttoSanzzo <konohittos@gmail.com>
2023-12-24 05:04:54 -0300

    minihell | AAAAA

--- srcs/ft_readline/ft_readline.c
+++ srcs/ft_readline/ft_readline.c
@@ -6,20 +6,11 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/08 09:13:14 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/12/08 09:33:51 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/12/24 05:04:48 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*ft_rl_jf(char *line, char *buffer)
-{
-	char	*new;
-
-	new = ft_strjoin(line, buffer);
-	free(line);
-	return (new);
-}
 
 char	*ft_readline(char *prompt)
 {
@@ -30,12 +21,17 @@ char	*ft_readline(char *prompt)
 	line = (char *)ft_calloc(2, sizeof(char));
 	if (!line)
 		return (NULL);
-	read(0, buffer, 1);
+	buffer[0] = 0;
+	if (read(0, buffer, 1) == 0)
+	{
+		free(line);
+		return (NULL);
+	}
 	while (*buffer != '\n')
 	{
-		line = ft_rl_jf(line, buffer);
-		if (read(0, buffer, 1) == -1)
-			break ;
+		line = ft_rejoin(line, buffer);
+		while (read(0, buffer, 1) == 0)
+			buffer[0] = 0;
 	}
 	return (line);
 }
