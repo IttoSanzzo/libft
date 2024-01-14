1be9978e0e1edf7faf841626b6e980a1c28ff055
marcosv2 <konohittos@gmail.com>
2024-01-14 14:05:03 -0300

    ft_strrem_bl

--- srcs/ft_put/ft_puttab.c
+++ srcs/ft_put/ft_puttab.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/20 14:44:50 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/12/20 14:59:42 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/14 14:04:47 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_puttab(char **tab, char *prompt)
 {
 	int	i;
 
+	if (!tab)
+		return ;
 	i = -1;
 	while (tab[++i])
 	{
