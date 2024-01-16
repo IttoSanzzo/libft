040dfa4d67a29ca2441fc75bbd40f5f47fb27b3a
IttoSanzzo <konohittos@gmail.com>
2024-01-16 17:11:36 -0300

    libft | ft_strtrim_xtoy

--- srcs/ft_put/ft_puttab.c
+++ srcs/ft_put/ft_puttab.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/20 14:44:50 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2024/01/14 14:30:32 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/16 17:11:35 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	ft_puttab(char **tab, char *prompt)
 	{
 		if (prompt)
 			ft_putstr(prompt);
-		ft_putendl_fd(tab[i], 1);
+		ft_putendl(tab[i]);
 	}
 }
