dc49986d55c607b7b3c7fa5fe8251990e34a4d1d
IttoSanzzo <konohittos@gmail.com>
2024-01-17 03:11:56 -0300

    libft | ft_strrem_xtoy

--- srcs/ft_str/ft_strtrim_xtoy.c
+++ srcs/ft_str/ft_strtrim_xtoy.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2024/01/16 16:34:09 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2024/01/17 03:09:50 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/17 03:11:55 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strtrim_xtoy(char *s, int x, int y)
 	if (!trim)
 		return (NULL);
 	i = -1;
-	while ((++i <= y) && (s[x + i]))
+	while (((x + ++i) <= y) && (s[x + i]))
 		trim[i] = s[x + i];
 	return (trim);
 }
