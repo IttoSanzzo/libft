d2bbbac440e5318d3b1d850ce7ad6f8ac384e70f
IttoSanzzo <konohittos@gmail.com>
2024-04-19 16:58:33 -0300

    libft | ft_strchrem (refix)

--- srcs/ft_str/ft_strchrem.c
+++ srcs/ft_str/ft_strchrem.c
@@ -6,29 +6,40 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:32:34 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/04/19 16:48:17 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/04/19 16:58:05 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_strchrem(char **str, char c)
+static int	ft_chrl(char *str, char c)
+{
+	int	i;
+	int	l;
+
+	i = -1;
+	l = 0;
+	while (str[++i])
+		if (str[i] != c)
+			l++;
+	return (l + 1);
+}
+
+char	*ft_strchrem(char **str, char c)
 {
 	char	*ret;
 	int		i;
+	int		j;
 
-	i = 0;
-	while (str[0][i])
-	{
-		if (str[0][i] == c)
-		{
-			*(str[0] + i) = *(str[0] + i + 1);
-			i++;
-		}
-		if (str[0][i])
-			i++;
-	}
-	ret = ft_strdup(*str);
+	if (!*str)
+		return (NULL);
+	i = -1;
+	i = -1;
+	ret = ft_calloc(ft_chrl(*str, c), sizeof(char));
+	while (str[0][++i])
+		if (str[0][i] != c)
+			ret[++j] = str[0][i];
 	ft_nfreestr(str);
 	*str = ret;
+	return (ret);
 }
