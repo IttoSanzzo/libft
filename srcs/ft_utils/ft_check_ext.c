c0b460e85bf84079c98d439af1f977f91a97d5b6
IttoSanzzo <konohittos@gmail.com>
2024-04-19 11:34:08 -0300

    libft | ft_check_ext (fix)

--- srcs/ft_utils/ft_check_ext.c
+++ srcs/ft_utils/ft_check_ext.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 11:27:17 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/04/19 11:30:11 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/04/19 11:33:56 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,11 @@ int	ft_check_ext(char *file, char *ext)
 	len = ft_strlen(ext) + 1;
 	i = ft_strlen(file);
 	while (file[--i])
-		if (file[i] == '.' && !ft_strncmp((char *)(file + i), file, len))
+	{
+		if (file[i] == '.' && !ft_strncmp((char *)(file + i + 1), file, len))
 			return (1);
+		else if (file[i] == '.')
+			return (1);
+	}
 	return (0);
 }
