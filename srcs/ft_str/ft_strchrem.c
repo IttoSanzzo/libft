6fd46da3811ed6d6390bde3163d606473de4bba2
IttoSanzzo <konohittos@gmail.com>
2024-04-19 16:48:34 -0300

    libft | ft_strchrem (fix)

--- srcs/ft_str/ft_strchrem.c
+++ srcs/ft_str/ft_strchrem.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:32:34 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/04/19 16:41:28 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/04/19 16:48:17 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,17 @@ void	ft_strchrem(char **str, char c)
 	char	*ret;
 	int		i;
 
-	i = -1;
-	while (str[0][++i])
+	i = 0;
+	while (str[0][i])
+	{
 		if (str[0][i] == c)
+		{
 			*(str[0] + i) = *(str[0] + i + 1);
+			i++;
+		}
+		if (str[0][i])
+			i++;
+	}
 	ret = ft_strdup(*str);
 	ft_nfreestr(str);
 	*str = ret;
